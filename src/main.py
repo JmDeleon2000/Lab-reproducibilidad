e7dbc6387f30a69332ffe968eb686126c30b4ac0
JmDeleon2000 <54918136+JmDeleon2000@users.noreply.github.com>
2022-12-14 14:49:49 -0600

    Revert "Añadir funcionalidad a main (causa un error al correr)"
    This reverts commit bfad5a90177a3a9f20e288e5dd9194805933a115.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from matplotlib import pyplot as plt
 import numpy as np
-from PIL import Image
 
 t = np.linspace(0, np.pi*50, 10000)
 
@@ -12,18 +11,4 @@ y = -3*np.sin(sqrt3*t)/sqrt3
 
 plt.figure(figsize=(20, 20))
 plt.plot(x, y)
-
-outPath = 'out/test.png'
-plt.savefig(outPath)
-
-img = Image.open(outPath)
-
-newData = []
-for item in img.getdata:
-    if item[0] == 255 and item[1] == 255 and item[2] == 255:
-        newData.append((255, 0, 0, 255))
-    else:
-        newData.append(item)
-
-img.putdata(newData)
-img.save(outPath, "PNG")
+plt.savefig('out/test.png')
