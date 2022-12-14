5b897d3b502c4294d4b63a2acdfc9bd5dc0fc721
JmDeleon2000 <54918136+JmDeleon2000@users.noreply.github.com>
2022-12-14 14:34:59 -0600

    cambiar a curvas de Lissajous

--- src/main.py
+++ src/main.py
@@ -3,8 +3,10 @@ import numpy as np
 
 t = np.linspace(0, np.pi*50, 10000)
 
-x = np.sin(t/9)**3
-y = np.cos(t)**2-np.sin(t*50)*np.cos(t*2)**2
+sqrt6 = np.sqrt(6)
+sqrt3 = np.sqrt(3)
+x = (np.cos(sqrt6*t)-np.cos(sqrt3*t)+5*np.sin(sqrt3*t)/sqrt3-34*np.sin(sqrt6*t)/sqrt6)/4
+y = -3*np.sin(sqrt3*t)/sqrt3
 
 
 plt.figure(figsize=(20, 20))
